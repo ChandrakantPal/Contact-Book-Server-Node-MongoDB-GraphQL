c5953b081e9c5b5932f50ad21c65d4ad50823ef2
ChandrakantPal <sanjay161097@gmail.com>
2021-03-22 02:39:05 +0530

    resolvers index

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,21 +1,13 @@
-const postsResolvers = require("./posts");
-const usersResolvers = require("./users");
-const commentsResolvers = require("./comments");
+const contactResolvers = require('./contacts')
+const usersResolvers = require('./users')
 
 module.exports = {
-  Post: {
-    likeCount: (parent) => parent.likes.length,
-    commentCount: (parent) => parent.comments.length,
-  },
   Query: {
-    ...postsResolvers.Query,
+    ...usersResolvers.Query,
+    ...contactResolvers.Query,
   },
   Mutation: {
     ...usersResolvers.Mutation,
-    ...postsResolvers.Mutation,
-    ...commentsResolvers.Mutation,
+    ...contactResolvers.Mutation,
   },
-  Subscription: {
-    ...postsResolvers.Subscription,
-  },
-};
+}
