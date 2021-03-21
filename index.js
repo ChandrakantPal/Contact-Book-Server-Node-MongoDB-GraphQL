ef77cb945b18e058d45235ac11bb64a8ac93ea9d
ChandrakantPal <sanjay161097@gmail.com>
2021-03-22 02:37:22 +0530

    cleanup

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const { ApolloServer, PubSub } = require("apollo-server");
-const mongoose = require("mongoose");
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
 
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
-const { MONGODB } = require("./config");
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+const { MONGODB } = require('./config')
 
-const pubsub = new PubSub();
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubsub }),
-});
+  context: ({ req }) => ({ req }),
+})
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to DB");
-    return server.listen({ port: PORT });
+    console.log('Connected to DB')
+    return server.listen({ port: PORT })
   })
   .then((res) => {
-    console.log(`Server running at ${res.url}`);
+    console.log(`Server running at ${res.url}`)
   })
   .catch((err) => {
-    console.error(err);
-  });
+    console.error(err)
+  })
